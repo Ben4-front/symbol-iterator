b8ba927642a6e84060fdfb5f29da04f13b749048
Ben4 <benymany511@gmail.com>
2025-10-28 14:07:05 +0300

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+import Team from './team.js';
+
+const team = new Team();
+
+// Пример персонажей
+const char1 = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10,
+};
+
+const char2 = {
+  name: 'Маг',
+  type: 'Magician',
+  health: 60,
+  level: 2,
+  attack: 30,
+  defence: 20,
+};
+
+team.add(char1);
+team.add(char2);
+
+for (const character of team) {
+  console.log(character);
+}
